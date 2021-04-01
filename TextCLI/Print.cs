4cfeff201135da43597bc0702274e5101dc8fa45
AnSt <starininandrey@gmail.com>
2021-04-01 15:18:17 +0300

    Add Print.cs

---
+++ TextCLI/Print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TextCLI
+{
+    public static class Print
+    {
+        public static void Info(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"{DateTime.Now:u} [ERROR] : {message}");
+            Console.ResetColor();
+        }
+        
+        
+    }
+}
